8c61ba6f5f528b99fddaadf9a96146fc9bd8c392
ease <3532753799@qq.com>
2018-05-13 00:25:19 +0800

    Signed-off-by: ease <3532753799@qq.com>

--- node-demo/server.js
+++ node-demo/server.js
@@ -4,7 +4,7 @@ var url = require('url')
 var port = process.argv[2]
 
 if(!port){
-  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+  console.log('请指定端口号好不啦？\n node server.js 8888 这样不会吗？')
   process.exit(1)
 }
 
@@ -17,11 +17,6 @@ var server = http.createServer(function(request, response){
   var queryObject = parsedUrl.query
   var method = request.method
 
-  /******** 从这里开始看，上面不要看 ************/
-
-// if (path == '/') {
-//
-// }
 
 
 
@@ -33,9 +28,9 @@ var server = http.createServer(function(request, response){
 
 
 
-  console.log('方方说：得到 HTTP 路径为\n' + path)
-  // console.log('方方说：查询字符串为\n' + query)
-  // console.log('方方说：不含查询字符串的路径为\n' + pathNoQuery)
+
+  console.log('得到 HTTP 路径为\n' + path)
+
 
   if(path == '/style.css'){
      response.setHeader('Content-Type', 'text/css; charset=utf-8')
@@ -64,8 +59,8 @@ var server = http.createServer(function(request, response){
 
 
 
-  /******** 代码结束，下面不要看 ************/
+
 })
 
 server.listen(port)
-console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+console.log('监听 ' + port + ' 成功\n请用打开 http://localhost:' + port)
